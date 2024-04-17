f0219c5221acbbe62499ef039638e5ccd12ef0cc
Gary Bright <garybright@rocketmail.com>
2024-04-17 12:09:01 -0400

    added base colors for tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./**/*.{html,handlebars}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "font": '#ffffff',
+        "primary": '#724DA3',
+        "secondary": '#E4B447',
+        "subtext": '#454544',
+        "linear": 'linear-gradient(0.25turn, #F9C932, #345CA1);',
+      },
+    },
   },
   plugins: [],
 };
