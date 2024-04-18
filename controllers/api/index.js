7a687d64276668e5958e6ead168d1f3a93cbdfe3
Gary Bright <garybright@rocketmail.com>
2024-04-18 11:08:29 -0400

    push signup route changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,6 @@ const battleRoutes = require('./battle-routes');
 const quizRoutes = require('./quiz-routes');
 const loginRouter = require('./login-routes');
 
-
 router.use('/login', loginRouter);
 router.use('/user', userRoutes);
 router.use('/battle', battleRoutes);
