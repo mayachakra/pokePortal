df4ff9efba09187411481fbbd19b12c8fb05befa
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-19 10:57:00 -0400

    preloader

--- public/script.js
+++ public/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World from JS!");
+const loader = document.querySelector('#loader');
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const header = document.querySelector('#header');
+    const pageContent = document.querySelector('#page-content');
+    loader.classList.add('hidden');
+    pageContent.classList.remove('hidden');
+    header.classList.remove('hidden');
+  }, 3000); // 3 seconds delay (3000 milliseconds)
+});
